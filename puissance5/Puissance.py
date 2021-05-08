d09fdb9d4858e172a769a7b49a77478d0cbef56c
TonyLim <tony-lim@hotmail.fr>
2021-05-08 11:31:28 +0200

    pruning and decision fixed

--- puissance5/Puissance.py
+++ puissance5/Puissance.py
@@ -10,12 +10,12 @@ def get_index_token_positionable(npboard, length=BOARD_LENGTH, height=BOARD_HEIG
     for x in range(length):
         for y in range(height-1, -1, -1):
             if npboard[y, x] == EMPTY_CELL:
-                indexes.append((y*length+x, (x, y)))
+                indexes.append((y*length+x, (y, x)))
                 break
     return indexes
 
 
-def minimax(tuple_board, depth, alpha, beta, is_red=True):
+def minimax(tuple_board, depth, alpha, beta, is_red):
     npboard = tuple_to_array(tuple_board)
     state = check_state(npboard)
     if depth == 0 or state != UNFINISHED_STATE:
@@ -43,38 +43,30 @@ def minimax(tuple_board, depth, alpha, beta, is_red=True):
                 break  # beta cutoff
         else:
             beta = min(beta, value)
-            if beta >= alpha:
+            if beta <= alpha:
                 break  # alpha cutoff
     scores[tuple_board] = value
     return value
 
 
-def decision(np_board, token, depth=5):
+def decision(np_board, token, depth=4):
     """
     return the position of the decision where the bot want to play his token
     :param np_board: board of the game of type numpy array
     :param token: RED_TOKEN if the bot is red YELLOW_TOKEN if the bot is yellow
     :param depth: height of the tree for the minimax algorithm set at 5 by default
-    :return: tuple (x,y) positions
+    :return: tuple (y,x) positions
     """
     bot_is_red = token == RED_TOKEN
     val, positions_token = -float('inf') if bot_is_red else float('inf'), (-1, -1)
     tuple_board = array_to_tuple(np_board)
-    minimax(tuple_board, depth, -float('inf'), float('inf'))
+    minimax(tuple_board, depth, -float('inf'), float('inf'), bot_is_red)
     for i, positions in get_index_token_positionable(np_board):
         child_board = update_tuple(tuple_board, i, token)
-        if bot_is_red and val < scores[child_board] or not bot_is_red and val > scores[child_board]:
-            val = scores[child_board]
+        if child_board not in scores:
+            continue
+        child_score = scores[child_board]
+        if bot_is_red and val < child_score or not bot_is_red and val > child_score:
+            val = child_score
             positions_token = positions
     return positions_token
-
-
-if __name__ == "__main__":
-    print('main launching')
-    gen_score()
-    minimax(TUPLE_ORIGINAL, 6, -float('inf'), float('inf'))
-    count = 0
-    for k, v in scores.items():
-        count += 1
-    print(scores)
-    print(count)
